ddbad2a1328eb5b687ceaddd17469b250f898465
pedeEli <74102959+pedeEli@users.noreply.github.com>
2022-04-12 02:47:35 +0200

    added tournament data structure

---
+++ src/app.d.ts
@@ -0,0 +1,79 @@
+/// <reference types="svelte"/>
+
+
+declare interface Tournament {
+    settings: Settings,
+    contestants: Contestants,
+    matches: Matches,
+    groups: Groups,
+    finales: Finales
+}
+
+declare interface Settings {
+    name: string,
+    state: 'mainMenu' | 'editingContestants' | 'editingGroups' | 'running',
+    haveGroups: boolean,
+    luckyLoser: boolean,
+    winnerPerGroup: number
+    addingContestant: {
+        addingType: 'person' | 'team',
+        teamName: string,
+        personName: string,
+        members: string[]
+    }
+}
+
+declare interface Contestants {
+    [id: string]: Contestant
+}
+
+declare type Contestant = Person | Team
+
+declare interface Person {
+    id: string,
+    type: 'person',
+    name: string
+}
+
+declare interface Team {
+    id: string,
+    type: 'team',
+    name: string,
+    members: string[]
+}
+
+declare interface Matches {
+    [id: string]: Match
+}
+
+declare interface Match {
+    id: string,
+    state: 'waiting' | 'running' | 'paused' | 'leftWon' | 'rightWon',
+    left: string,
+    right: string
+    leftScore: number,
+    rightScore: number
+}
+
+declare interface Groups {
+    [id: string]: Group
+}
+
+declare interface Group {
+    id: string,
+    name: string
+    members: string[],
+    matches: string[]
+}
+
+declare interface Finales {
+    [id: string]: Finale
+}
+
+declare interface Finale {
+    id: string
+    parentId: string,
+    matchId: string
+}
+
+declare type Writable<T> = import('svelte/store').Writable<T>
